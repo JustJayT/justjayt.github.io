584be6ad830ebf868f9475a743a3f94ed0a614a1
JustJayT <wufflesinblue@gmail.com>
2025-10-21 19:11:50 -0700

    done

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/resume')
+def index():
+    return render_template('resume.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
